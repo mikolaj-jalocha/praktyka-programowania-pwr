f319c594ee298dea5bfc9a7c59cf4bcd4163cf87
Mikołaj Jałocha <76820915+mikolaj-jalocha@users.noreply.github.com>
2026-04-23 19:29:26 +0200

    Add bridge pattern implementation in bridge.py
    Implement bridge pattern with color and shape classes.

--- design-patterns/bridge.py
+++ design-patterns/bridge.py
@@ -1,1 +1,53 @@
+from abc import ABC, abstractmethod
 
+import numpy as np
+import time
+import os
+
+
+class Color(ABC):
+    @abstractmethod
+    def pokoloruj(self) -> str:
+        pass
+
+class RedColor(Color):
+    def pokoloruj(self) -> str:
+        return "czerwony"
+
+class BlueColor(Color):
+    def pokoloruj(self) -> str:
+        return "niebieski"
+
+class Shape:
+
+    def __init__(self, color: Color):
+        self.color = color
+
+    @abstractmethod
+    def show(self):
+        pass
+
+class Rectangle(Shape):
+
+    def show(self):
+        print("Tu narysuje prostokat w kolorze: {self.color.pokoloruj()}")
+
+
+class Triangle(Shape):
+
+    def show(self):
+        print("Tu narysuje trojkat w kolorze: {self.color.pokoloruj()}")
+
+def main():
+    red = RedColor()
+    blue = BlueColor()
+
+    red_rectangle = Rectangle(red)
+    blue_triangle = Triangle(blue)
+
+
+    blue_triangle.show()
+
+
+if __name__ == "__main__":
+    main()
